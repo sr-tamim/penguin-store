a979f47c03fddbf39a9e0e1718caf807003180ba
TAMIM <saifur.tamim@gmail.com>
2021-09-12 23:23:44 +0600

    rating showed

--- js/app.js
+++ js/app.js
@@ -18,7 +18,9 @@ const showProducts = (products) => {
     <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
+      <h4>Category: ${product.category}</h4>
+      <h5>Average rating: ${product.rating.rate}/5.0</h5>
+      <h5>Rated by: ${product.rating.count} users</h5>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
