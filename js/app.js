db691499238b146b32435a553dce433a439fd029
TAMIM <saifur.tamim@gmail.com>
2021-09-13 02:24:23 +0600

    total amount bug fixed

--- js/app.js
+++ js/app.js
@@ -90,7 +90,7 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
 
 
